220eed601b9da6c504a414841d64d139b3a02b41
Max-Zhu <zhusheng003@163.com>
2015-05-12 11:17:53 +0800

    Create pre_process.py

---
+++ pre_process.py
@@ -0,0 +1,21 @@
+import re
+import nltk
+
+stopwords = nltk.corpus.stopwords.words('english')
+porter_stemmer = nltk.stem.porter.PorterStemmer()
+re_pattern = r'''([A-Z]\.)+[A-Z]?|\s+[^&]\w+([-']\w+)*|\$?\d+(\.\d+)?%?\w*'''
+
+def normalize_twitter(text):
+    """normalization for twitter"""
+    text = re.sub(r'(RT|@|https?:\/\/)[^ ]+|#', '', text)
+    text = re.sub(r'(^| )[:;x]-?[\(\)dop]($| )', ' ', text)  # facemark
+    text = re.sub(r'(^| )(rt[ :]+)*', ' ', text)
+    text = re.sub(r'([hj])+([aieo])+(\1+\2+){1,}', r'\1\2\1\2', text, re.IGNORECASE)  # laugh
+    text = re.sub(r' +(via|live on) *$', '', text)
+    return text
+
+def tokenize_stemm(text):
+	text = re_pattern.sub('', text)
+	words = nltk.tokenize.word_tokenize(text)
+	stem_words = [porter_stemmer.stem_word(word) for word in words if word.lower() not in stopwords]
+	return stem_words
