62c947efee629973935cdb1a0a617abbfcb3023a
Max-Zhu <zhusheng003@163.com>
2015-05-14 11:20:52 +0800

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,21 +1,59 @@
-import re
+#-*-coding:utf-8-*-
+import re, sys, codecs, pprint
 import nltk
+from gensim import corpora, models, similarities
+import logging
+from nltk.corpus import wordnet as wn
+
+all_noun = [lemma.name for synset in wn.all_synsets('n') for lemma in synset.lemmas]
+all_verb = [lemma.name for synset in wn.all_synsets('v') for lemma in synset.lemmas]
+corpus = all_noun
+corpus.extend(all_verb)
 
 stopwords = nltk.corpus.stopwords.words('english')
-porter_stemmer = nltk.stem.porter.PorterStemmer()
-re_pattern = r'''([A-Z]\.)+[A-Z]?|\s+[^&]\w+([-']\w+)*|\$?\d+(\.\d+)?%?\w*'''
+stemmer = nltk.stem.PorterStemmer()
+re_pattern = r'''([A-Z]\.)+[A-Z]?|\w+([-']\w+)*|\$?\d+(\.\d+)?%?\w*'''
 
 def normalize_twitter(text):
     """normalization for twitter"""
-    text = re.sub(r'(RT|@|https?:\/\/)[^ ]+|#', '', text)
+    text = re.sub(r'(@|https?:\/\/)[^ ]+|#|RT', '', text)
     text = re.sub(r'(^| )[:;x]-?[\(\)dop]($| )', ' ', text)  # facemark
     text = re.sub(r'(^| )(rt[ :]+)*', ' ', text)
+    text = re.sub(r'\s+&\w+', ' ', text)
     text = re.sub(r'([hj])+([aieo])+(\1+\2+){1,}', r'\1\2\1\2', text, re.IGNORECASE)  # laugh
     text = re.sub(r' +(via|live on) *$', '', text)
     return text
 
 def tokenize_stemm(text):
 	text = normalize_twitter(text)
-	words = nltk.regexp_tokenize(text, re_pattern)
-	stem_words = [porter_stemmer.stem_word(word) for word in words if word.lower() not in stopwords]
-	return stem_words
+	words = [word.lower() for word in nltk.regexp_tokenize(text, re_pattern)]
+	words_stemmed = [stemmer.stem(word) for word in words if word not in stopwords]
+	words_filtered = [word for word in words_stemmed if word in corpus]
+	return words_filtered
+
+def init_model():
+	documents = [line.strip() for line in open('/home/max/Ipython/Workspace/test.txt')]
+	texts = [tokenize_stemm(document) for document in documents]
+	#pprint.pprint(texts)
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+	dictionary = corpora.Dictionary([corpus])
+	docs = [dictionary.doc2bow(text) for text in texts]
+	tfidf = models.TfidfModel(docs)
+
+	docs_tfidf = tfidf[docs]
+	index = similarities.SparseMatrixSimilarity(docs_tfidf, num_features = len(corpus))
+	index.save('./profiles.index')
+
+	return dictionary, tfidf
+
+def tfidf_test():
+	dictionary, tfidf = init_model()
+	index = similarities.SparseMatrixSimilarity.load('./profiles.index')
+
+	query = tokenize_stemm("@sIeepingpiIls I love it I can wait for spring break")
+	query_vec = dictionary.doc2bow(query)
+	sims = index[tfidf[query_vec]]
+	
+	result = sorted(enumerate(sims), key=lambda item: -item[1])
+	pprint.pprint(result)
